13ed3258a63ac4d96496af8286d39e5e75ec6db1
Pema Sherpa <pematamang111@gmail.com>
2024-04-09 22:37:32 -0400

    add crud in routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,21 +11,29 @@ router.get('/', async(req, res) => {
   res.json(categories);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async(req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const category = await Category.findOne(req.params.id);
+  res.json(category);
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+  const newCategory = await Category.create(req.body);
+  res.json(newCategory);
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a category by its `id` value
+  const updateCategory = await Category.update(req.body.id);
+  res.json(updateCategory);
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
+  const deleteCategory = await Category.destroy(req.body.id);
+  res.json(deleteCategory);
 });
 
 module.exports = router;
